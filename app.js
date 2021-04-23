92de06d7474edd94d611571a6b5fc808fbcd7ff2
changjandam <changjandamtw@gmail.com>
2021-04-23 19:44:34 +0800

    Complete dynamic routing

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ app.get('/', (req, res) => {
   res.render('index', { movie: movies.results })
 })
 
+app.get('/movies/:movie_id', (req, res) => {
+ 
+  const movie = movies.results.filter(movie => movie.id == req.params.movie_id)
 
+  res.render('show', { movie: movie[0] })
+
+})
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:3000`)
 })
